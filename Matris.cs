bc1b65d40cd93d7b78032df68b0eb045029d7860
irfan ayaz <byirfanayaz@gmail.com>
2024-02-19 23:19:35 +0300

    Kosegen Matris eklendi

--- Matris.cs
+++ Matris.cs
@@ -32,5 +32,42 @@ public class Matris
             System.Console.WriteLine();
         }
     }
+    /// <summary>
+    /// Sıfır matrisi oluşturur
+    /// </summary>
+    /// <param name="satir">Satır</param>
+    /// <param name="sutun">Sutun</param>
+    /// <returns>Sıfır Matrisi</returns>
+    public static int [,] SifirMatrisiOlustur(int satir, int sutun)
+    {
+       return Olustur(satir,sutun,0,0);
+    }
+
+    /// <summary>
+    /// Birler matrisi oluşturur
+    /// </summary>
+    /// <param name="satir">Satır</param>
+    /// <param name="sutun">Sutun</param>
+    /// <returns>Birler Matrisi</returns>
+    public static int [,] BirlerMatrisiOlustur(int satir, int sutun)
+    {
+       return Olustur(satir,sutun,1,1);
+    }
+
+    /// <summary>
+    /// Kosegen Matris Olusturur
+    /// </summary>
+    /// <param name="boyut">Satır ve Sutun Sayısı</param>
+    /// <param name="min">Minumum değeler</param>
+    /// <param name="max">Maximumum değeler</param>
+    /// <returns>Kosegen Matris/returns>
+    public static int [,] KosegenMatrisOlustur (int boyut=3, int min = 1, int max =9)
+    {
+        int [,] X = SifirMatrisiOlustur(boyut, boyut);
+        for (int i = 0; i < X.GetLength(0); i++)
+            X[i,i] = new Random ().Next(min , max);      
+        return X;
+
+    }
 
 }
